ce5dc8efe4550edf0fb592e7e2433a2c140fd082
FROFY <karos-76@yandex.ru>
2024-04-18 16:55:57 +0300

    inits

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: { preflight: false }
+} satisfies Config
+
